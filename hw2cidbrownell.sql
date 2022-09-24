d1fcb420dbfa0b767fa080427d3599a78893e8cc
cidb123 <cbrownell2772@sdsu.edu>
2022-09-23 20:13:58 -0700

    Uploading Homework

--- hw2cidbrownell.sql
+++ hw2cidbrownell.sql
@@ -1,24 +1,46 @@
 
-use baseball;
-
-show tables;
+USE baseball;
 
 ALTER TABLE inning MODIFY COLUMN game_id INT UNSIGNED NOT NULL;
 
+
 ### Historical Average
-CREATE TABLE IF NOT EXISTS Historical_Average (Batter INT NOT NULL, Average Float(4,3)) ENGINE=MyISAM
-   SELECT  batter, SUM(Hit)/SUM(atBat) AS Average FROM batter_counts group by batter;
+
+DROP TABLE IF EXISTS Historical_Average;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS Historical_Average (Batter INT NOT NULL, Average Float(4,3)) ENGINE=MyISAM
+   SELECT  batter, SUM(Hit)/SUM(atBat) AS Average FROM batter_counts GROUP BY batter;
+
 
 #### Annual Average
-CREATE TABLE IF NOT EXISTS Annual_Average (Year INT, Batter INT NOT NULL, Average Float(4,3)) ENGINE=MyISAM
+
+DROP TABLE IF EXISTS Annual_Average;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS Annual_Average (Year INT, Batter INT NOT NULL, Average Float(4,3)) ENGINE=MyISAM
     SELECT YEAR(g.local_date) AS Year, b.batter, SUM(b.Hit)/SUM(b.atBat) AS Average
-    FROM batter_counts b join game g ON g.game_id = b.game_id
-    group by YEAR(g.local_date), b.batter;
-
-##### Average over Last 100 days, Currently doesn't accurately gather last 100 days, been stuck on this for hours
-SELECT  DATE(g.local_date) as game_date, b.game_id, b.batter, SUM(b.Hit)/SUM(b.atBat) as Average
-FROM    game g join batter_counts b on g.game_id = b.game_id
-WHERE   g.local_date BETWEEN g.local_date - INTERVAL 100 DAY AND g.local_date
-group by g.local_date, b.batter;
+    FROM batter_counts b JOIN game g ON g.game_id = b.game_id
+    GROUP BY b.batter, YEAR(g.local_date);
 
 
+##### Average over Last 100 days
+
+DROP TABLE IF EXISTS temp_for_rolling;
+
+CREATE TEMPORARY TABLE temp_for_rolling
+    SELECT b.batter, b.Hit ,b.atBat ,b.game_id, (b.Hit)/(NULLIF(b.atBat, 0)) AS avg FROM batter_counts b
+    GROUP BY b.batter;
+
+DROP TABLE IF EXISTS ROLLING_AVG;
+
+CREATE TEMPORARY TABLE IF NOT EXISTS ROLLING_AVG(game_date date, Batter INT NOT NULL, Average Float(4,3)) ENGINE=MyISAM
+    SELECT  DATE(g.local_date) AS game_date, s.game_id, s.batter, AVG(s.avg) OVER (ORDER BY DATE(g.local_date)
+          ROWS BETWEEN 100 PRECEDING AND 1 PRECEDING) AS Average
+    FROM    game g JOIN temp_for_rolling s ON g.game_id = s.game_id
+    GROUP BY game_date, s.batter;
+
+
+SELECT * FROM ROLLING_AVG LIMIT 0, 200;
+
+SELECT * FROM Annual_Average LIMIT 0, 200;
+
+SELECT * FROM Historical_Average LIMIT 0, 200;
