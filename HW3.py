46fb54e55e8203820b77967977ad902e0866c190
cidb123 <cbrownell2772@sdsu.edu>
2022-10-07 17:38:18 -0700

    Uploading HW

--- HW3.py
+++ HW3.py
@@ -1,49 +1,80 @@
-from pyspark.ml import Pipeline
+import sys
+
+from pyspark import StorageLevel
+from pyspark.ml.feature import CountVectorizer
 from pyspark.sql import SparkSession
 
-from split_column_transform import SplitColumnTransform
 
-appName = "PySpark Example - MariaDB Example"
-master = "local"
-spark = SparkSession.builder.appName(appName).master(master).getOrCreate()
+def main():
+    appname = "HW3"
+    master = "local"
+    spark = SparkSession.builder.appName(appname).master(master).getOrCreate()
 
-sql = """
-SELECT DATE(g.local_date) AS game_date, s.game_id,
-        s.batter, AVG(s.avg) OVER (ORDER BY DATE(g.local_date)
+    sql = """SELECT DATE(g.local_date) AS game_date, b.game_id,
+        b.batter, AVG(SUM(b.Hit)/SUM(b.atbat)) OVER (ORDER BY DATE(g.local_date)
         ROWS BETWEEN 100 PRECEDING AND 1 PRECEDING)
             AS Average
 FROM    game g
-    JOIN temp_for_rolling s
-        ON g.game_id = s.game_id
-GROUP BY game_date, s.batter;"""
-database = "baseball"
-user = "username"
-# pragma: allowlist nextline secret
-password = "password"
-server = "localhost"
-port = 3306
-jdbc_url = f"jdbc:mysql://{server}:{port}/{database}?permitMysqlScheme"
-jdbc_driver = "org.mariadb.jdbc.Driver"
+    JOIN  batter_counts b
+        ON g.game_id = b.game_id
+GROUP BY game_date, b.batter;
+"""
+    database = "baseball"
+    user = "root"
+    # pragma: allowlist nextline secret
+    password = "root"
+    server = "localhost"
+    port = 3306
+    jdbc_url = f"jdbc:mysql://{server}:{port}/{database}?permitMysqlScheme"
+    jdbc_driver = "org.mariadb.jdbc.Driver"
 
-df = (
-    spark.read.format("jdbc")
-    .option("url", jdbc_url)
-    .option("query", sql)
-    .option("user", user)
-    .option("password", password)
-    .option("driver", jdbc_driver)
-    .load()
-)
+    df = (
+        spark.read.format("jdbc")
+        .option("url", jdbc_url)
+        .option("query", sql)
+        .option("user", user)
+        .option("password", password)
+        .option("driver", jdbc_driver)
+        .load()
+    )
 
-split_column_transform = SplitColumnTransform(
-    inputCols=["class", "sex", "embarked", "who"], outputCol="categorical"
-)
-split_column_transform = SplitColumnTransform(
-    inputCols=["class", "sex", "embarked", "who"], outputCol="categorical"
-)
+    df.write.options(header="true").csv("baseball_spark.csv")
 
-pipeline = Pipeline(stages=[split_column_transform])
-model = pipeline.fit(df)
-df = model.transform(df)
+    rolling_df = spark.read.csv("baseball_spark.csv", inferSchema="true", header="true")
+    rolling_df.createOrReplaceTempView("titanic")
+    rolling_df.persist(StorageLevel.MEMORY_ONLY)
 
-df.show()
+    rolling_df = spark.sql(
+        """
+        SELECT
+                *
+                , SPLIT(CONCAT(
+                    CASE WHEN game_date IS NULL THEN ""
+                    ELSE game_date END,
+                    " ",
+                    CASE WHEN game_id IS NULL THEN ""
+                    ELSE game_id END,
+                    " ",
+                    CASE WHEN batter IS NULL THEN ""
+                    ELSE batter END,
+                    " ",
+                    CASE WHEN Average IS NULL THEN ""
+                    ELSE Average END
+                ), " ") AS categorical
+            FROM baseball_spark
+        """
+    )
+    rolling_df.show()
+
+    count_vectorized = CountVectorizer(
+        inputCol="categorical", outputCol="categorical_vector"
+    )
+    count_vectorized_fitted = count_vectorized.fit(rolling_df)
+    rolling_df = count_vectorized_fitted.transform(rolling_df)
+    rolling_df.show()
+
+    return
+
+
+if __name__ == "__main__":
+    sys.exit(main())
